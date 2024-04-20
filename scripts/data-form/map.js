444b5d97daaab781c25e3edddd4fa14803ee61d0
portulans <solenn.tual@ensg.eu>
2024-04-20 13:17:17 +0200

    Add X-Y edition on map function

--- scripts/data-form/map.js
+++ scripts/data-form/map.js
@@ -83,3 +83,71 @@ L.control.layers.tree(baseLayers, overLayers, {
   namedToggle: true,
   collapsed:false
 }).addTo(map);
+
+
+////////// FORM COMPLETION PART //////////////
+
+var marker = null; // Variable to store marker instance
+var markerAdded = false; // Variable to track if marker is added
+
+function chooseMarkerLocation(){
+  var xcoord = document.getElementById('object-coord-x').value.trim();
+  var ycoord = document.getElementById('object-coord-y').value.trim();
+
+  if (xcoord === "" && ycoord === "") {
+    return markercoords = [0, 0]
+  } else {
+    return markercoords = [xcoord, ycoord]
+  }
+}
+
+function addMarker() {
+  if (marker === null) {
+    var position = chooseMarkerLocation()
+    marker = L.marker(position, { draggable: true }).addTo(map);
+
+    marker.on('move', function(e) {
+      var lat = marker.getLatLng().lat.toFixed(6);
+      var lng = marker.getLatLng().lng.toFixed(6);
+      marker.bindPopup("X: " + lng + "<br>Y: " + lat).openPopup();
+    });
+
+    markerAdded = true; // Update markerAdded variable
+    updateButtonState(); // Update button state
+  }
+}
+
+function removeMarker() {
+  if (marker !== null) {
+    map.removeLayer(marker);
+    marker = null;
+    markerAdded = false; // Update markerAdded to false
+    updateButtonState(); // Update button state
+  }
+}
+
+function updateXYCoordsInForm() {
+  // Send the coordinate of the marker to the HTML form (X and X inputs)
+  if (marker !== null) {
+    var lat = marker.getLatLng().lat.toFixed(6);
+    var lng = marker.getLatLng().lng.toFixed(6);
+    document.getElementById('object-coord-x').value = lat;
+    document.getElementById('object-coord-y').value = lng;
+  }
+}
+
+function updateButtonState() {
+  // Enable/Disable Add marker and Remove marker button, to never have more than one marker on the map
+  var addButton = document.getElementById('xyaddmarker');
+  var removeButton = document.getElementById('xyremovemarker');
+  var sendValueButton = document.getElementById('xytoform');
+  
+  addButton.disabled = markerAdded; // Disable add marker button if marker is added
+  removeButton.disabled = !markerAdded; // Disable remove marker button if marker is not added
+  sendValueButton.disabled = !markerAdded; // Disable send data to form button if marker is not added
+}
+
+document.getElementById('xyaddmarker').addEventListener('click', addMarker);
+document.getElementById('xytoform').addEventListener('click', updateXYCoordsInForm);
+document.getElementById('xyremovemarker').addEventListener('click', removeMarker);
+updateButtonState(); // Call initially to set button state
