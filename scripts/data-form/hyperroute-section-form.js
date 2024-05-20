0a973f44a7fdda601fe89bb97f66430afefe7b39
Wac <xtram13@gmail.com>
2024-05-20 13:08:11 +0200

    hyperroute form : ergonomics - sections

--- scripts/data-form/hyperroute-section-form.js
+++ scripts/data-form/hyperroute-section-form.js
@@ -48,33 +48,34 @@ function addHyperrouteSectionDivOnForm(sectionDataArray) {
   addInputHiddenFieldToHyperrouteSection(firstColumnDiv, "hyperroute-section-id", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.ID]));
   // Second column
   const secondColumnDiv =  document.createElement("div");
+  secondColumnDiv.classList.add("grid-wrapper-fixed-3-columns");
   // First line
-  const firstLineDiv =  document.createElement("div");
-  addInputTextFieldToHyperrouteSection(firstLineDiv, "hyperroute-section-sorting-id", "Sorting ID", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.HUMAN_ID]), "Sorting/Human ID", true);
-  addSelect2FieldToHyperrouteSection(firstLineDiv, "hyperroute-section-location-a-id", "Location A", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.LOCATION_A_ID]), "A Section End (Astronomical Object)");
-  addSelect2FieldToHyperrouteSection(firstLineDiv, "hyperroute-section-location-b-id", "Location B", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.LOCATION_B_ID]), "B Section End (Astronomical Object)");
-  addInputNumberFieldToHyperrouteSection(firstLineDiv, 0.01, "hyperroute-section-date-from", "Date : From", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.DATE_FROM]), "Start date");
-  addInputNumberFieldToHyperrouteSection(firstLineDiv, 0.01, "hyperroute-section-date-to", "To", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.DATE_TO]), "End date");
+  // const firstLineDiv =  document.createElement("div");
+  addInputTextFieldToHyperrouteSection(secondColumnDiv, "grid-column-one field-wrapper", "hyperroute-section-sorting-id", "Sorting ID", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.HUMAN_ID]), "Sorting/Human ID", true);
+  addInputCheckboxFieldToHyperrouteSection(secondColumnDiv, "grid-column-two field-wrapper", "hyperroute-section-data-certified", "Publish on Map", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.is_certified]), "Are data fully checked or sufficient, and ready for publication ?");
+  addSelect2FieldToHyperrouteSection(secondColumnDiv, "grid-column-one-to-three field-wrapper", "hyperroute-section-location-a-id", "Location A", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.LOCATION_A_ID]), "A Section End (Astronomical Object)");
+  addSelect2FieldToHyperrouteSection(secondColumnDiv, "grid-column-one-to-three field-wrapper", "hyperroute-section-location-b-id", "Location B", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.LOCATION_B_ID]), "B Section End (Astronomical Object)");
+  addInputNumberFieldToHyperrouteSection(secondColumnDiv, 0.01, "grid-column-one field-wrapper", "hyperroute-section-date-from", "Date : From", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.DATE_FROM]), "Start date");
+  addInputNumberFieldToHyperrouteSection(secondColumnDiv, 0.01, "grid-column-two field-wrapper", "hyperroute-section-date-to", "To", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.DATE_TO]), "End date");
   // Second line
-  const secondLineDiv =  document.createElement("div");
-  addInputCheckboxFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-canon", "Canon", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.CANON]), "Is Hyperroute Section Canon (\"© Disney\" canon) ?");
-  addInputCheckboxFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-legends", "Legends", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.LEGENDS]), "Is Hyperroute Section Legends (original/old canon) ?"
+  // const secondLineDiv =  document.createElement("div");
+  addInputCheckboxFieldToHyperrouteSection(secondColumnDiv, "grid-column-one field-wrapper", "hyperroute-section-canon", "Canon", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.CANON]), "Is Hyperroute Section Canon (\"© Disney\" canon) ?");
+  addInputCheckboxFieldToHyperrouteSection(secondColumnDiv, "grid-column-two field-wrapper", "hyperroute-section-legends", "Legends", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.LEGENDS]), "Is Hyperroute Section Legends (original/old canon) ?"
   );
-  addInputCheckboxFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-unlicenced", "Unlicenced", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.UNLICENSED]), "Is Hyperroute Section unlicenced (neither Canon nor Legends) ?");
-  addTravelTimeFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-travel-time", "Section Travel Time", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.AVERAGE_TRAVEL_TIME]), "Average travel time in days, hours, minutes, seconds");
-  addTextAreaFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-desc", "Desc", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.DESC]), "Hyperroute Section Description");
-  addInputTextFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-placement-certitude", "Placement Certitude", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.PLACEMENT_CERTITUDE]), "Indicate Hyperroute Section placement certitude");
-  addTextAreaFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-placement-logic", "Placement Logic", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.PLACEMENT_LOGIC]), "Explain object placement logic");
-  addTextAreaFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-notes", "Notes", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.NOTES]), "Notes");
-  addTextAreaFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-interesting", "Interesting", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.INTERESTING]), "Additionnal elements of interest");
-  addInputCheckboxFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-data-certified", "Publish on map ?", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.is_certified]), "Are data fully checked or sufficient, and ready for publication ?");
-  addTextAreaFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-geom", "GEOM", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.GEOM]), "Geometry for complex objects like lines, polygons …", true);
-  addInputTextFieldToHyperrouteSection(secondLineDiv, "hyperroute-section-geom-type", "GEOM TYPE", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.GEOM_TYPE]), "Used to build Geojson files.");
+  addInputCheckboxFieldToHyperrouteSection(secondColumnDiv, "grid-column-three field-wrapper", "hyperroute-section-unlicenced", "Unlicenced", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.UNLICENSED]), "Is Hyperroute Section unlicenced (neither Canon nor Legends) ?");
+  addTravelTimeFieldToHyperrouteSection(secondColumnDiv, "grid-column-one-to-three field-wrapper", "hyperroute-section-travel-time", "Section Travel Time", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.AVERAGE_TRAVEL_TIME]), "Average travel time in days, hours, minutes, seconds");
+  addTextAreaFieldToHyperrouteSection(secondColumnDiv, "grid-column-one-to-three field-wrapper", "hyperroute-section-desc", "Desc", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.DESC]), "Hyperroute Section Description");
+  addInputTextFieldToHyperrouteSection(secondColumnDiv, "grid-column-one field-wrapper", "hyperroute-section-placement-certitude", "Placement Certitude", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.PLACEMENT_CERTITUDE]), "Indicate Hyperroute Section placement certitude");
+  addTextAreaFieldToHyperrouteSection(secondColumnDiv, "grid-column-two-to-three field-wrapper", "hyperroute-section-placement-logic", "Placement Logic", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.PLACEMENT_LOGIC]), "Explain object placement logic");
+  addTextAreaFieldToHyperrouteSection(secondColumnDiv, "grid-column-one-to-three field-wrapper", "hyperroute-section-notes", "Notes", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.NOTES]), "Notes");
+  addTextAreaFieldToHyperrouteSection(secondColumnDiv, "grid-column-one-to-three field-wrapper", "hyperroute-section-interesting", "Interesting", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.INTERESTING]), "Additionnal elements of interest");
+  addTextAreaFieldToHyperrouteSection(secondColumnDiv, "grid-column-one-to-two field-wrapper", "hyperroute-section-geom", "GEOM", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.GEOM]), "Geometry for complex objects like lines, polygons …", true);
+  addInputTextFieldToHyperrouteSection(secondColumnDiv, "grid-column-three field-wrapper", "hyperroute-section-geom-type", "GEOM TYPE", sanitizeText(sectionDataArray[HYPERROUTE_SECTION_COLUMN.GEOM_TYPE]), "Used to build Geojson files.");
 
   // Append elements
   rootDiv.appendChild(firstColumnDiv);
-  secondColumnDiv.appendChild(firstLineDiv);
-  secondColumnDiv.appendChild(secondLineDiv);
+  // secondColumnDiv.appendChild(firstLineDiv);
+  // secondColumnDiv.appendChild(secondLineDiv);
   rootDiv.appendChild(secondColumnDiv);
   HYPERROUTE_SECTION_FORM_CONTENT.appendChild(rootDiv);
 }
@@ -151,17 +152,17 @@ async function deleteAllHyperrouteSectionRowByHyperrouteId(hyperrouteId) {
   return sectionStatus;
 }
 
-function addInputHiddenFieldToHyperrouteSection(parentDiv, className, value) {
+function addInputHiddenFieldToHyperrouteSection(parentDiv, fieldClassName, value) {
   // Field
   let field = document.createElement("input");
   field.type = "hidden";
-  field.classList.add(className);
+  field.classList.add(fieldClassName);
   field.value = value;
   // Append fields
   parentDiv.appendChild(field);
 }
 
-function addInputTextFieldToHyperrouteSection(parentDiv, className, label, value, tooltip, isReadonly=false) {
+function addInputTextFieldToHyperrouteSection(parentDiv, wrapperClassName, fieldClassName, label, value, tooltip, isReadonly=false) {
   // Label
   let labelField = document.createElement("label");
   labelField.innerHTML = label;
@@ -169,16 +170,17 @@ function addInputTextFieldToHyperrouteSection(parentDiv, className, label, value
   let field = document.createElement("input");
   field.type = "text";
   field.readOnly = isReadonly;
-  field.classList.add(className);
+  field.classList.add(fieldClassName);
   field.value = value;
   // Append fields
   let container = addFieldTooltiToHyperrouteSection(tooltip);
-  labelField.appendChild(field);
+  wrapperClassName.split(" ").forEach(className => container.classList.add(className));
   container.appendChild(labelField);
+  container.appendChild(field);
   parentDiv.appendChild(container);
 }
 
-function addInputNumberFieldToHyperrouteSection(parentDiv, rangeStep, className, label, value, tooltip, isReadonly=false) {
+function addInputNumberFieldToHyperrouteSection(parentDiv, rangeStep, wrapperClassName, fieldClassName, label, value, tooltip, isReadonly=false) {
   // Label
   let labelField = document.createElement("label");
   labelField.innerHTML = label;
@@ -187,31 +189,33 @@ function addInputNumberFieldToHyperrouteSection(parentDiv, rangeStep, className,
   field.type = "number";
   field.step = rangeStep;
   field.readOnly = isReadonly;
-  field.classList.add(className);
+  field.classList.add(fieldClassName);
   field.value = value;
   // Append fields
   let container = addFieldTooltiToHyperrouteSection(tooltip);
-  labelField.appendChild(field);
+  wrapperClassName.split(" ").forEach(className => container.classList.add(className));
   container.appendChild(labelField);
+  container.appendChild(field);
   parentDiv.appendChild(container);
 }
 
-function addSelect2FieldToHyperrouteSection(parentDiv, className, label, value, tooltip) {
+function addSelect2FieldToHyperrouteSection(parentDiv, wrapperClassName, fieldClassName, label, value, tooltip) {
   // Label
   let labelField = document.createElement("label");
   labelField.innerHTML = label;
   // Field
   let field = document.createElement("select");
-  field.classList.add(className);
+  field.classList.add(fieldClassName);
   loadLocationSelect2ToHyperrouteSection(field, value);
   // Append fields
   let container = addFieldTooltiToHyperrouteSection(tooltip);
-  labelField.appendChild(field);
+  wrapperClassName.split(" ").forEach(className => container.classList.add(className));
   container.appendChild(labelField);
+  container.appendChild(field);
   parentDiv.appendChild(container);
 }
 
-function addInputCheckboxFieldToHyperrouteSection(parentDiv, className, label, value, tooltip, isReadonly=false) {
+function addInputCheckboxFieldToHyperrouteSection(parentDiv, wrapperClassName, fieldClassName, label, value, tooltip, isReadonly=false) {
   // Label
   let labelField = document.createElement("label");
   labelField.innerHTML = label;
@@ -219,32 +223,34 @@ function addInputCheckboxFieldToHyperrouteSection(parentDiv, className, label, v
   let field = document.createElement("input");
   field.type = "checkbox";
   field.readOnly = isReadonly;
-  field.classList.add(className);
+  field.classList.add(fieldClassName);
   setCheckboxElementStateFromValue(field, value, PREFORMATED_VALUES.YES_NO_EMPTY_ARRAY);
   // Append fields
   let container = addFieldTooltiToHyperrouteSection(tooltip);
-  labelField.appendChild(field);
+  wrapperClassName.split(" ").forEach(className => container.classList.add(className));
   container.appendChild(labelField);
+  container.appendChild(field);
   parentDiv.appendChild(container);
 }
 
-function addTextAreaFieldToHyperrouteSection(parentDiv, className, label, value, tooltip, isReadonly=false) {
+function addTextAreaFieldToHyperrouteSection(parentDiv, wrapperClassName, fieldClassName, label, value, tooltip, isReadonly=false) {
   // Label
   let labelField = document.createElement("label");
   labelField.innerHTML = label;
   // Field
   let field = document.createElement("textarea");
   field.readOnly = isReadonly;
-  field.classList.add(className);
+  field.classList.add(fieldClassName);
   field.value = value;
   // Append fields
   let container = addFieldTooltiToHyperrouteSection(tooltip);
-  labelField.appendChild(field);
+  wrapperClassName.split(" ").forEach(className => container.classList.add(className));
   container.appendChild(labelField);
+  container.appendChild(field);
   parentDiv.appendChild(container);
 }
 
-function addTravelTimeFieldToHyperrouteSection(parentDiv, className, label, value, tooltip, isReadonly=false) {
+function addTravelTimeFieldToHyperrouteSection(parentDiv, wrapperClassName, fieldClassName, label, value, tooltip, isReadonly=false) {
   // Label
   let labelField = document.createElement("label");
   labelField.innerHTML = label;
@@ -254,37 +260,38 @@ function addTravelTimeFieldToHyperrouteSection(parentDiv, className, label, valu
   let dayField = document.createElement("input");
   dayField.type = "number";
   dayField.readOnly = isReadonly;
-  dayField.classList.add(className+"-day");
+  dayField.classList.add(fieldClassName+"-day");
   dayField.min = 0;
   dayField.value = duration[0];
   let hourField = document.createElement("input");
   hourField.type = "number";
   hourField.readOnly = isReadonly;
-  hourField.classList.add(className+"-hour");
+  hourField.classList.add(fieldClassName+"-hour");
   hourField.min = 0;
   hourField.max = 23;
   hourField.value = duration[1];
   let minuteField = document.createElement("input");
   minuteField.type = "number";
   minuteField.readOnly = isReadonly;
-  minuteField.classList.add(className+"-minute");
+  minuteField.classList.add(fieldClassName+"-minute");
   minuteField.min = 0;
   minuteField.max = 59;
   minuteField.value = duration[2];
   let secondField = document.createElement("input");
   secondField.type = "number";
   secondField.readOnly = isReadonly;
-  secondField.classList.add(className+"-second");
+  secondField.classList.add(fieldClassName+"-second");
   secondField.min = 0;
   secondField.max = 59;
   secondField.value = duration[3];
   // Append fields
   let container = addFieldTooltiToHyperrouteSection(tooltip);
-  labelField.appendChild(dayField);
-  labelField.appendChild(hourField);
-  labelField.appendChild(minuteField);
-  labelField.appendChild(secondField);
+  wrapperClassName.split(" ").forEach(className => container.classList.add(className));
   container.appendChild(labelField);
+  container.appendChild(dayField);
+  container.appendChild(hourField);
+  container.appendChild(minuteField);
+  container.appendChild(secondField);
   parentDiv.appendChild(container);
 }
 
