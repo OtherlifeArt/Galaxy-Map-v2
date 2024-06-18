f1b4d72e455385fe1146a60c9b0e008cd95764b8
user <user@debian>
2024-06-18 14:10:42 +0200

    System generator - Restructuring - WIP

---
+++ scripts/data-form/wizard-system-generator-canvas.js
@@ -0,0 +1,83 @@
+/**
+ * Draw (again) all canvas elements
+ */
+function drawCanvas() {
+  
+}
+
+/**
+ * 
+ */
+function drawTextInfoOnCanvas () {
+
+}
+
+/**
+ * Draw orbit
+ */
+function drawOrbitOnVanvas () {
+
+}
+
+/**
+ * Draw planet
+ */
+function drawPlanetOnCanvas () {
+
+}
+
+/**
+ * Draw ring
+ */
+function drawRingOnCanvas () {
+
+}
+
+/**
+ * Draw star
+ */
+function drawStarOnCanvas () {
+
+}
+
+/**
+ * Draw comet
+ */
+function drawCometOnCanvas () {
+
+}
+
+/**
+ * 
+ */
+function drawAsteroidOnCanvas () {
+
+}
+
+/**
+ * Draw fields
+ */
+function drawFieldOnCanvas () {
+
+}
+
+/**
+ * 
+ */
+function drawHillSphereOnCanvas () {
+
+}
+
+/**
+ * 
+ */
+function drawCircumstellarHabitableZoneOnCanvas () {
+
+}
+
+/**
+ * Draw circle
+ */
+function drawCircleOnCanvas () {
+
+}
